fad3b75584547a5c1103355a7b6e069c864df764
Antoine Pietri <antoine.pietri1@gmail.com>
2018-10-05 12:53:33 +0200

    Add requirements-test.txt

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+import os
 from setuptools import setup, find_packages
 
 
-def parse_requirements():
-    requirements = []
-    for reqf in ('requirements.txt', 'requirements-swh.txt'):
-        with open(reqf) as f:
-            for line in f.readlines():
-                line = line.strip()
-                if not line or line.startswith('#'):
-                    continue
-                requirements.append(line)
+def parse_requirements(name=None):
+    if name:
+        reqf = 'requirements-%s.txt' % name
+    else:
+        reqf = 'requirements.txt'
 
+    requirements = []
+    if not os.path.exists(reqf):
+        return requirements
+
+    with open(reqf) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+            requirements.append(line)
     return requirements
 
 
@@ -22,8 +29,9 @@ setup(
     url='https://forge.softwareheritage.org/source/swh-mirror-forge/',
     packages=find_packages(),
     scripts=[],
-    install_requires=parse_requirements(),
+    install_requires=parse_requirements() + parse_requirements('swh'),
     setup_requires=['vcversioner'],
+    extras_require={'testing': parse_requirements('test')},
     vcversioner={},
     include_package_data=True,
 )
